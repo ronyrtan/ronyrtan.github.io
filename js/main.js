06eecc57012db4edf32f41169dc41b24d2799a00
ronyrtan <ron.yr.tan@hotmail.com>
2019-08-29 00:29:54 +1000

    test for about page

--- js/main.js
+++ js/main.js
@@ -10,20 +10,20 @@ $(document).ready(function(){
       $('nav').css('background', 'rgba(0,0,0,0)');
     }
 
-    if ($(document).scrollTop() > 100) {
-      $('.about h1').animate({left: '0px'},1000);
-      $('.about h1').addClass('fadeIn');
-    }
-    if ($(document).scrollTop() > 300) {
-      $('.about p').animate({left: '0px'},1000);
-      $('.about p').addClass('fadeIn');
-    }
+    // if ($(document).scrollTop() > 100) {
+    //   $('.about h1').animate({left: '0px'},1000);
+    //   $('.about h1').addClass('fadeIn');
+    // }
+    // if ($(document).scrollTop() > 300) {
+    //   $('.about p').animate({left: '0px'},1000);
+    //   $('.about p').addClass('fadeIn');
+    // }
   });
 });
 
 $(function(){
   $(".element").typed({
-    strings: ["Hi there! Welcome to my page.^500", "I'm a ^200 Web Designer.^1000", "A ^200 Front-End Web Developer.^1000", "I am ^300 .^300 .^300 .^300", "<strong>RON TAN.</strong>"],
+    strings: ["Hi there! ^100", "I'm a developer^100", "Turned ^500 UX Designer.^1000", "I am ^300 .^300 .^300 .^300", "<strong>RON TAN.</strong>"],
     typeSpeed: 50,
     startDelay: 5,
     backSpeed: 0,
@@ -33,8 +33,6 @@ $(function(){
     }
   });
 });
-
-
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
